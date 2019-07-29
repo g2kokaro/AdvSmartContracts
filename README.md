d8aa12d9c7e6136816fed5f897ac0237cdd9cc5c
Dima Kokarovtsev <Dmytro.Kokarovtsev@georgebrown.ca>
2019-07-28 23:08:13 -0400

    Contract bug fixes. Added more info to readme.

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 This is a set of smart contracts for tüb, our decentralized video platform. The contracts allow content creators to crowdfund their upcoming projects, similar to kickstarter.
 
-The concepts used are multiple contract interactions and upgradeable smart contracts.
+The concepts used are multiple contract interactions, separate storage and logic contracts, and upgradeable smart contracts. The Registry contract [is deployed here](https://kovan.etherscan.io/address/0xde342587b6eec0ab5ecf85bc4fd2792c614509ee), and the CrowdFund contract [is deployed here](https://kovan.etherscan.io/address/0xc746b51ef3e6e929ae6c67e843c555f45da7d051).
 
+#### References
+The contracts are based on [Stu's crowdfunding example from class](https://gist.github.com/stupeters187/7418acc1e1bf4edd5f423c0d8d86e8c2) and [Bernard Peh's proxy contract article](https://medium.com/@blockchain101/the-basics-of-upgradable-proxy-contracts-in-ethereum-479b5d3363d6).
--- TubCrowdFund.sol
+++ TubCrowdFund.sol
@@ -10,17 +10,17 @@ contract Storage {
         uint weiRaised;
         mapping (address => uint) contributions;
     }
-    
+
     mapping(uint => CrowdFund) crowdFunds;
     uint public currentFundId;
 }
 
 contract CrowdFund is Storage {
-    
+
     event NewContribution(address indexed _who, uint _amount, uint _timestamp);
     event BeneficiaryPaid(address _beneficiary, uint _amount, uint _timestamp);
     event newCrowdFundCreated(uint _id, address payable _channelAddress, uint _goalInWei, uint _deadline);
-    
+
     function newCrowdFund(address payable _channelAddress, uint _goalInWei, uint _duration, string memory _description) public {
         crowdFunds[currentFundId].channelAddress = _channelAddress;
         crowdFunds[currentFundId].goalInWei = _goalInWei;
@@ -29,24 +29,25 @@ contract CrowdFund is Storage {
         emit newCrowdFundCreated(currentFundId, _channelAddress, _goalInWei, now + _duration);
         currentFundId++;
     }
-    
+
     function pledge(uint _id) public payable {
-        require(now < crowdFunds[_id].deadline);
+        require(now < crowdFunds[_id].deadline, "Contributions can only be made before the deadline");
         if (crowdFunds[_id].contributions[tx.origin] == 0){
             crowdFunds[_id].contributors++;
-        } 
+        }
         crowdFunds[_id].contributions[tx.origin] += msg.value;
+        crowdFunds[_id].weiRaised += msg.value;
         emit NewContribution(msg.sender, msg.value, now);
     }
-    
+
     function refund(uint _id) public {
-        require(address(this).balance < crowdFunds[_id].goalInWei && now > crowdFunds[_id].deadline, 
+        require(crowdFunds[_id].weiRaised < crowdFunds[_id].goalInWei && now > crowdFunds[_id].deadline,
             "Refunds are only allowed if the goal has not been met by the deadline.");
         uint amountToSend = crowdFunds[_id].contributions[tx.origin];
         crowdFunds[_id].contributions[tx.origin] = 0;
         tx.origin.transfer(amountToSend);
     }
-    
+
     function payChannelOwner(uint _id) public {
         require(address(this).balance >= crowdFunds[_id].goalInWei && now > crowdFunds[_id].deadline,
             "The funds can only be collected if the goal has been reached by the deadline.");
@@ -54,22 +55,26 @@ contract CrowdFund is Storage {
         crowdFunds[_id].channelAddress.transfer(address(this).balance);
         emit BeneficiaryPaid(crowdFunds[_id].channelAddress, amountToSend, now);
     }
-    
+
     function getBalance(uint _id) public view returns (uint) {
         return crowdFunds[_id].weiRaised;
+    }
+
+    function getContributors(uint _id) public view returns (uint) {
+        return crowdFunds[_id].contributors;
     }
 }
 
 contract Registry is Storage {
-    
+
     address delegate;
     address owner;
-    
+
     constructor() public {
         owner = msg.sender;
     }
-    
-    function () payable external {
+
+    function () external payable {
         require(delegate != address(0x0), "Delegate contract address must be set.");
         address target = delegate;
         assembly {
@@ -83,11 +88,10 @@ contract Registry is Storage {
             case 1 { return(ptr, size) }
         }
     }
-    
+
     function setDelegateAddress (address _c) public {
-        require(msg.sender == owner);
+        require(msg.sender == owner, "Only the owner can set the delegate address");
         delegate = _c;
     }
-    
-}
 
+}
